194295d4106b7b7e7ebdba6ae339fefe0856c791
Akindejoye <tydruhill@gmail.com>
2023-04-14 13:55:39 +0100

    Environment variables and Response Interface

---
+++ models/NewsArticles.ts
@@ -0,0 +1,13 @@
+export interface NewsResponse {
+  articles: NewsArticle[],
+}
+
+export interface NewsArticle {
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage?: string;
+  publishedAt: string;
+  content: string;
+}
