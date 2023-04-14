8426f493ba23b26f6b064a23a013901ec7de38f5
Akindejoye <tydruhill@gmail.com>
2023-04-14 20:35:36 +0100

    Search component and Search api

--- components/NewsArticleGrid.tsx
+++ components/NewsArticleGrid.tsx
@@ -3,19 +3,19 @@ import { Col, Row } from "react-bootstrap";
 import NewsArticleEntry from "./NewsArticleEntry";
 
 interface NewsArticlesGridProps {
-  articles: NewsArticle[];
+    articles: NewsArticle[],
 }
 
 const NewsArticlesGrid = ({ articles }: NewsArticlesGridProps) => {
-  return ( 
-    <Row xs={1} sm={2} xl={3} className="g-4">
-      {articles.map(article => (
-        <Col key={article.url}>
-          <NewsArticleEntry article={article} />
-        </Col>
-      ))}
-    </Row>
-   );
+    return (
+        <Row xs={1} sm={2} xl={3} className="g-4">
+          {articles.map(article => (
+              <Col key={article.url}>
+                  <NewsArticleEntry article={article} />
+              </Col>
+          ))}
+        </Row>
+    );
 }
- 
+
 export default NewsArticlesGrid;
